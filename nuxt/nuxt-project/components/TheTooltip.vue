1a1b898584bb463d9e7b8eac659586323e4c43f4
elhamzeinodini <elhammth@gmail.com>
2025-08-05 19:16:34 +0330

    trim classes

--- nuxt/nuxt-project/components/TheTooltip.vue
+++ nuxt/nuxt-project/components/TheTooltip.vue
@@ -33,83 +33,53 @@ const showTooltipContent = ref(false)
 
 /// ///////////////////////////////////////////////////////////////////////////////////////// methods
 const getArrowPlacement = (placement: TooltipPlacement) => {
-  switch (placement) {
-    case 'top':
-      return 'absolute top-[1.8rem] left-[40%]'
-    case 'top-end':
-      return 'absolute top-[1.8rem] left-[60%]'
-    case 'top-start':
-      return 'absolute top-[1.8rem] left-[30%]'
-    case 'bottom':
-      return 'absolute -top-[0.35rem] left-[2rem]'
-    case 'bottom-end':
-      return 'absolute -top-[0.35rem] left-[4.5rem]'
-    case 'bottom-start':
-      return 'absolute -top-[0.35rem] left-[0.5rem]'
-    case 'right':
-      return 'absolute right-[10rem] top-[0.5rem]'
-    case 'right-end':
-      return 'absolute right-[10rem] top-[0.9rem]'
-    case 'right-start':
-      return 'absolute right-[10rem] top-[0.2rem]'
-    case 'left':
-      return 'absolute left-[10rem] top-[0.6rem]'
-    case 'left-end':
-      return 'absolute left-[10rem] top-[0.9rem]'
-    case 'left-start':
-      return 'absolute left-[10rem] top-[0.3rem]'
-    default:
-      return null
+  const positionMap = {
+    top: 'absolute top-[1.8rem] left-[40%]',
+    'top-end': 'absolute top-[1.8rem] left-[60%]',
+    'top-start': 'absolute top-[1.8rem] left-[30%]',
+    bottom: 'absolute -top-[0.35rem] left-[2rem]',
+    'bottom-end': 'absolute -top-[0.35rem] left-[4.5rem]',
+    'bottom-start': 'absolute -top-[0.35rem] left-[0.5rem]',
+    right: 'absolute right-[10rem] top-[0.5rem]',
+    'right-end': 'absolute right-[10rem] top-[0.9rem]',
+    'right-start': 'absolute right-[10rem] top-[0.2rem]',
+    left: 'absolute left-[10rem] top-[0.6rem]',
+    'left-end': 'absolute left-[10rem] top-[0.9rem]',
+    'left-start': 'absolute left-[10rem] top-[0.3rem]',
   }
+
+  return positionMap[placement] ?? ''
 }
 
 
 const getPlacement = (placement: TooltipPlacement) => {
-  switch (placement) {
-    case 'top':
-    case 'top-end':
-    case 'top-start':
-      return 'absolute -top-[2.4rem] left-[-3rem]' 
-    case 'right':
-    case 'right-end':
-    case 'right-start':
-      return 'absolute -right-[10.5rem] top-[0.2rem]'  
-    case 'bottom':
-    case 'bottom-end':
-    case 'bottom-start':
-      return 'mt-[0.5rem]'  
-    case 'left':
-    case 'left-end':
-    case 'left-start':
-      return 'absolute -left-[10.5rem] -top-[0.1rem]' 
-    default:
-      return null 
+  const positionMap = {
+    top: 'absolute -top-[2.4rem] left-[-3rem]',
+    'top-end': 'absolute -top-[2.4rem] left-[-3rem]',
+    'top-start': 'absolute -top-[2.4rem] left-[-3rem]',
+    right: 'absolute -right-[10.5rem] top-[0.2rem]',
+    'right-end': 'absolute -right-[10.5rem] top-[0.2rem]',
+    'right-start': 'absolute -right-[10.5rem] top-[0.2rem]',
+    bottom: 'mt-[0.5rem]',
+    'bottom-end': 'mt-[0.5rem]',
+    'bottom-start': 'mt-[0.5rem]',
+    left: 'absolute -left-[10.5rem] -top-[0.1rem]',
+    'left-end': 'absolute -left-[10.5rem] -top-[0.1rem]',
+    'left-start': 'absolute -left-[10.5rem] -top-[0.1rem]',
   }
+
+  return positionMap[placement] ?? ''
 }
 
 const getArrow = (placement: TooltipPlacement) => {
-  switch (placement) {
-    case 'top':
-    case 'top-start':
-    case 'top-end':
-      return 'border-l-6 border-r-6 border-t-6 border-l-transparent border-r-transparent'
+  const topArrow = 'border-l-6 border-r-6 border-t-6 border-l-transparent border-r-transparent'
+  const bottomArrow = 'border-l-6 border-l-transparent border-r-6 border-r-transparent border-b-6'
+  const rightArrow = 'border-t-6 border-b-6 border-r-6 border-t-transparent border-b-transparent'
+  const leftArrow = 'border-t-6 border-b-6 border-l-6 border-t-transparent border-b-transparent'
 
-    case 'bottom':
-    case 'bottom-end':
-    case 'bottom-start':
-      return 'border-l-6 border-l-transparent border-r-6 border-r-transparent border-b-6'
-
-    case 'right':
-    case 'right-end':
-    case 'right-start':
-      return 'border-t-6 border-b-6 border-r-6 border-t-transparent border-b-transparent'
-
-    case 'left':
-    case 'left-end':
-    case 'left-start':
-      return 'border-t-6 border-b-6 border-l-6 border-t-transparent border-b-transparent'
-    default:
-      return null
-  }
+  if (placement.includes('top')) return topArrow
+  else if (placement.includes('right')) return rightArrow
+  else if (placement.includes('bottom')) return bottomArrow
+  else return leftArrow
 }
 </script>
