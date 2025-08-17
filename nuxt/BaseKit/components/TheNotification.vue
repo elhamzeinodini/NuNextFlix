4bf6312182cd0d3fedbc119c94c631853a5f9418
elhamzeinodini <elhammth@gmail.com>
2025-08-17 16:38:31 +0330

    init notification

---
+++ nuxt/BaseKit/components/TheNotification.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="the-notification">
+    <div class="left">
+      <slot />
+    </div>
+
+    <button @click="emit('close')">x</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+/// /////////////////////////////////////////////////////////////////////////////////////// emits
+const emit = defineEmits(["close"]);
+
+/// /////////////////////////////////////////////////////////////////////////////////////// types
+type Type = "primary" | "success" | "warning" | "info" | "danger";
+
+type Position = "top-right" | "bottom-right" | "bottom-left" | "top-left";
+
+interface Props {
+  autoClose: boolean;
+  type: Type;
+  position: Position;
+  showCloseBtn: boolean;
+}
+
+/// /////////////////////////////////////////////////////////////////////////////////////// props
+const props = withDefaults(defineProps<Props>(), {
+  autoClose: false,
+  showCloseBtn: true,
+});
+</script>
